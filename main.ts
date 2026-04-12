2af000028d6f8c8d009c7439a999b69638252766
vboxuser <t.h.law@lancaster.ac.uk>
2026-04-12 00:09:10 +0000

    auto trim spaces at start/end or when having 3+ continuous spaces in string, user can now interrupt in the middle of string display and change to other strings, modes remain in the same index after reswitching back instead of starting from beginning

--- main.ts
+++ main.ts
@@ -1,21 +1,67 @@
-function displayPattern (payload: string) 
+let currentImageIndex = 0
+let currentStringIndex = 0
+let currentIndex = 0
+let IMGMode = true
+let ImageList: string[] = []
+let StringList: string[] = []
+let rawData = ""
+let cardExist = false
+let displayChange = false
+
+
+function cleanString(payload: string): string 
 {
-    basic.clearScreen()
-    for (let i = 0; i <= 24; i++) 
+    let result = ""
+    let spaceCount = 0
+
+    // trim the start and end if there are spaces
+    let start = 0
+    while (start < payload.length && payload.charAt(start) == " ") 
     {
-        if (payload.charAt(i) == "1") 
+        start += 1
+    }
+    let end = payload.length - 1
+    while (end >= start && payload.charAt(end) == " ") 
+    {
+        end -= 1
+    }
+
+    for (let i = start; i <= end; i++) 
+    {
+        let character = payload.charAt(i)
+
+        if (character == " ") 
         {
-            let x = i % 5
-            let y = Math.idiv(i, 5)
-            led.plot(x, y)
+            spaceCount += 1
+        } else 
+        {
+            if (spaceCount >= 3) 
+            {
+                result += " "
+            } else 
+            {
+                for (let j = 0; j < spaceCount; j++) 
+                {
+                    result += " "
+                }
+            }
+            spaceCount = 0
+            result += character
         }
     }
+
+    // handle spaces at very end of trimmed region if needed
+    if (spaceCount >= 3) {
+        result += " "
+    } else {
+        for (let j = 0; j < spaceCount; j++) {
+            result += " "
+        }
+    }
+
+    return result
 }
-function displaystring (payload: string) 
-{
-    basic.clearScreen()
-    basic.showString(payload, 100)
-}
+
 // visual image to tell user the NFC card is reading
 function loadingdata () 
 {
@@ -33,11 +79,156 @@ function loadingdata ()
     led.unplot(2, 3)
     led.unplot(1, 2)
 }
+
+function displayString(payload: string) 
+{
+    let myToken = displayChange
+    basic.clearScreen()
+
+    for (let i = 0; i < payload.length; i++) 
+    {
+        if (myToken != displayChange) 
+        {
+            return
+        }
+
+        basic.showString(payload.charAt(i), 80)
+
+        if (myToken != displayChange) 
+        {
+            return
+        }
+    }
+    basic.clearScreen()
+    basic.showIcon(IconNames.Yes)
+}
+
+function displayPattern(payload: string) 
+{
+    let myToken = displayChange
+    if (myToken != displayChange) 
+    {
+        return
+    }
+
+    basic.clearScreen()
+    for (let i = 0; i <= 24; i++) 
+    {
+        if (payload.charAt(i) == "1") 
+        {
+            let x = i % 5
+            let y = Math.idiv(i, 5)
+            led.plot(x, y)
+        }
+    }
+
+    basic.clearScreen()
+    basic.showIcon(IconNames.Yes)
+}
+
+function showCurrentItem() 
+{
+    let activeList = IMGMode ? ImageList : StringList
+    let index = IMGMode ? currentImageIndex : currentStringIndex
+
+    if (activeList.length <= 0) 
+    {
+        basic.showIcon(IconNames.No)
+        return
+    }
+
+    if (index >= activeList.length) 
+    {
+        index = 0
+        if (IMGMode) 
+        {
+            currentImageIndex = 0
+        } else 
+        {
+            currentStringIndex = 0
+        }
+    }
+
+    if (index < 0)
+    {
+        index = activeList.length - 1
+        if (IMGMode) 
+        {
+            currentImageIndex = activeList.length - 1
+        } else 
+        {
+            currentStringIndex = activeList.length - 1
+        }
+    }
+
+    displayChange = !displayChange
+    let myToken = displayChange
+
+    control.inBackground(function () 
+    {
+        basic.showNumber((index + 1), 100)
+
+        if (myToken != displayChange) {
+            return
+        }
+
+        if (IMGMode) 
+        {
+            displayPattern(activeList[index])
+        } else 
+        {
+            displayString(activeList[index])
+        }
+    })
+}
+
+function moveCurrentIndex(step: number) 
+{
+    let activeList = IMGMode ? ImageList : StringList
+
+    if (activeList.length <= 0) 
+    {
+        basic.showIcon(IconNames.No)
+        return
+    }
+
+    let index = IMGMode ? currentImageIndex : currentStringIndex
+    index += step
+
+    if (index >= activeList.length) 
+    {
+        index = 0
+    }
+
+    if (index < 0) 
+    {
+        index = activeList.length - 1
+    }
+
+    if (IMGMode) 
+    {
+        currentImageIndex = index
+    } else 
+    {
+        currentStringIndex = index
+    }
+    showCurrentItem()
+}
+
+input.onButtonPressed(Button.A, function () 
+{
+    moveCurrentIndex(-1)
+})
+
+input.onButtonPressed(Button.B, function () 
+{
+    moveCurrentIndex(1)
+})
+
 input.onButtonPressed(Button.AB, function () 
 {
-    // to switch between true and false
-    IMGMode = !(IMGMode)
-    // visual feedback
+    IMGMode = !IMGMode
+
     if (IMGMode) 
     {
         basic.showString("I")
@@ -45,39 +236,9 @@ input.onButtonPressed(Button.AB, function ()
     {
         basic.showString("S")
     }
+    showCurrentItem()
 })
-input.onButtonPressed(Button.B, function () 
-{
-    let activeList = IMGMode ? ImageList : StringList
-    if (activeList.length > 0) 
-    {
-        currentIndex += 1
-        // wrap around if over the last item
-        if (currentIndex >= activeList.length) 
-        {
-            currentIndex = 0
-        }
 
-        // Display data based on the current mode
-        if (IMGMode) 
-        {
-            displayPattern(activeList[currentIndex])
-        } else 
-        {
-            displaystring(activeList[currentIndex])
-        }
-        serial.writeLine("Switching to index: " + currentIndex)
-    } 
-    else 
-    {
-        basic.showIcon(IconNames.No)
-    }
-})
-let currentIndex = 0
-let IMGMode = true
-let ImageList: string[] = []
-let StringList: string[] = []
-let rawData = ""
 MFRC522.Init()
 basic.showIcon(IconNames.SmallDiamond)
 
@@ -86,77 +247,135 @@ basic.forever(function ()
     
     if (MFRC522.RequestStatus(0x26) == 0) 
     {
-        rawData=""
-        rawData = MFRC522.read()
-        serial.writeLine(rawData)
-        if (rawData.length > 0) 
+        if (!cardExist)
         {
-            loadingdata()
-            // resets the stored data on micrbit to the newest NFC tag
-            StringList = []
-            ImageList = []
-            currentIndex = 0
-            let imagePosition = 0
-            let stringPosition =0 
-            
-            serial.writeLine("Current string position is " + stringPosition)
-            while (rawData.indexOf("IMG", imagePosition) != -1) 
+            cardExist = true
+            rawData=""
+            rawData = MFRC522.read()
+            serial.writeLine(rawData)
+            if (rawData.length > 0) 
             {
-                // the datablock of image
-                let found = rawData.indexOf("IMG", imagePosition)
-                if (found != -1) 
+                loadingdata()
+                // resets the stored data on micrbit to the newest NFC tag
+                StringList = []
+                ImageList = []
+                currentIndex = 0
+                let imagePosition = 0
+                let stringPosition =0 
+                
+                /*
+                serial.writeLine("Current string position is " + stringPosition)
+                while (rawData.indexOf("IMG", imagePosition) != -1) 
                 {
-                    let marker = rawData.indexOf("en", found)
-                    if (marker != -1) 
+                    // the datablock of image
+                    let found = rawData.indexOf("IMG", imagePosition)
+                    if (found != -1) 
                     {
-                        serial.writeLine("found an IMG datablock")
-                        let dataStart = marker + 2
-                        ImageList.push(rawData.substr(dataStart, 25))
-                        // end of datebyte, continue searching for image type
-                        imagePosition = dataStart + 25
-                    } else {
-                        imagePosition = found + 3 //move past "IMG"
+                        let marker = rawData.indexOf("en", found)
+                        if (marker != -1) 
+                        {
+                            serial.writeLine("found an IMG datablock")
+                            let dataStart = marker + 2
+                            ImageList.push(rawData.substr(dataStart, 25))
+                            // end of datebyte, continue searching for image type
+                            imagePosition = dataStart + 25
+                        } else {
+                            imagePosition = found + 3 //move past "IMG"
+                        }
                     }
+
                 }
-
-            }
-            while (rawData.indexOf("STRING", stringPosition) != -1)
-            {
-                let found2 = rawData.indexOf("STRING", stringPosition)
-                let marker2 = rawData.indexOf("en", found2)
-
-                if (marker2 != -1)
+                while (rawData.indexOf("STRING", stringPosition) != -1)
                 {
+                    let found2 = rawData.indexOf("STRING", stringPosition)
+                    let marker2 = rawData.indexOf("en", found2)
+
+                    if (marker2 != -1)
+                    {
+                        let dataStart = marker2 + 2
+                        StringList.push(rawData.substr(dataStart, 25))
+                        stringPosition = dataStart + 25
+                    }else
+                    {
+                        stringPosition = found2 + 6
+                    }
+
+                }
+                */
+                while (true) 
+                {
+                    let found2 = rawData.indexOf("STRING", stringPosition)
+                    if (found2 == -1) break
+
+                    let marker2 = rawData.indexOf("en", found2)
+                    if (marker2 == -1) {
+                        stringPosition = found2 + 6
+                        continue
+                    }
+
                     let dataStart = marker2 + 2
-                    StringList.push(rawData.substr(dataStart, 25))
-                    stringPosition = dataStart + 25
-                }else
-                {
-                    stringPosition = found2 + 6
-                }
+                    if (dataStart + 25 > rawData.length) break
 
-            }
-            // if in imagemode, display first image data
-            // if not imagemode, display strings
-            // if have data in other mode, auto switch
-            if (IMGMode && ImageList.length > 0) 
-            {
-                displayPattern(ImageList[0])
-            } else if (!(IMGMode) && StringList.length > 0) 
-            {
-                displaystring(StringList[0])
-                serial.writeLine("Displayed String 0")
-            } else if (ImageList.length > 0)
-            {
-                IMGMode = !IMGMode
-                displayPattern(ImageList[0])
-            } else if (StringList.length > 0)
-            {
-                IMGMode = !IMGMode
-                displayPattern(StringList[0])
+                    let payload = rawData.substr(dataStart, 25)
+                    payload = cleanString(payload)
+                    StringList.push(payload)
+
+                    stringPosition = dataStart + 25
+                }
+                while (true) {
+                    let found = rawData.indexOf("IMG", imagePosition)
+                    if (found == -1) break
+
+                    let marker = rawData.indexOf("en", found)
+                    if (marker == -1) {
+                        imagePosition = found + 3
+                        continue
+                    }
+
+                    let dataStart = marker + 2
+                    if (dataStart + 25 > rawData.length) break
+
+                    let payload = rawData.substr(dataStart, 25)
+
+                    // optional validation: only 0/1 allowed
+                    let valid = true
+                    for (let i = 0; i < payload.length; i++) {
+                        if (payload.charAt(i) != "0" && payload.charAt(i) != "1") {
+                            valid = false
+                            break
+                        }
+                    }
+
+                    if (valid) {
+                        ImageList.push(payload)
+                    }
+
+                    imagePosition = dataStart + 25
+                }
+                // if in imagemode, display first image data
+                // if not imagemode, display strings
+                // if have data in other mode, auto switch
+                if (IMGMode && ImageList.length > 0) 
+                {
+                    displayPattern(ImageList[0])
+                } else if (!(IMGMode) && StringList.length > 0) 
+                {
+                    displayString(StringList[0])
+                    serial.writeLine("Displayed String 0")
+                } else if (ImageList.length > 0)
+                {
+                    IMGMode = !IMGMode
+                    displayPattern(ImageList[0])
+                } else if (StringList.length > 0)
+                {
+                    IMGMode = false
+                    displayString(StringList[0])
+                }
             }
         }
-        rawData = ""
+    } else 
+    {
+        cardExist = false
     }
     basic.pause(500)
 })
